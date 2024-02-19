0e8ced08f3f3a1141d24c1a0bcaa78f9b2f5f012
Dp <jaredpitiu1709@gmail.com>
2024-02-19 14:13:06 -0800

    Codigo estable

--- Main.py
+++ Main.py
@@ -1,10 +1,21 @@
 from tarea import Tarea
 from lista_de_tareas import ListaDeTareas
+import os
+import sys, signal, pyfiglet
+
+def limpiar_pantalla():
+    # Comando para limpiar la pantalla según el sistema operativo
+    if os.name == 'posix':
+        _ = os.system('clear')
+    else:
+        _ = os.system('cls')
 
 def main():
     lista_de_tareas = ListaDeTareas()
 
     while True:
+        limpiar_pantalla()
+        pyfiglet.print_figlet(text="task hero", colors='BLUE', font="isometric1")
         print("\n--- Aplicación de Lista de Tareas ---")
         print("1. Agregar Tarea")
         print("2. Marcar Tarea como Completada")
@@ -20,6 +31,7 @@ def main():
             nueva_tarea = Tarea(descripcion)
             lista_de_tareas.agregar_tarea(nueva_tarea)
             print("[+] ¡Tarea agregada exitosamente!")
+            pausar_pantalla()
 
         elif eleccion == "2":
             print("Tareas:")
@@ -27,6 +39,7 @@ def main():
             indice_tarea = int(input("Ingresa el índice de la tarea para marcar como completada: ")) - 1
             lista_de_tareas.tareas[indice_tarea].marcar_como_completada()
             print("[+] ¡Tarea marcada como completada!")
+            pausar_pantalla()
 
         elif eleccion == "3":
             print("Tareas:")
@@ -34,10 +47,12 @@ def main():
             indice_tarea = int(input("Ingresa el índice de la tarea para eliminar: ")) - 1
             lista_de_tareas.eliminar_tarea(indice_tarea)
             print("[+] ¡Tarea eliminada exitosamente!")
+            pausar_pantalla()
 
         elif eleccion == "4":
             print("Tareas:")
             print(lista_de_tareas)
+            pausar_pantalla()
 
         elif eleccion == "5":
             tareas_completadas = lista_de_tareas.obtener_reporte_de_tareas(completadas=True)
@@ -45,15 +60,22 @@ def main():
                 print("[+] Tareas Completadas:")
                 for tarea in tareas_completadas:
                     print(tarea)
+                    pausar_pantalla()
             else:
                 print("[!] No hay tareas completadas.")
+                pausar_pantalla()
 
         elif eleccion == "6":
             print("[!] Saliendo...")
+            pausar_pantalla()
             break
 
         else:
             print("[!] Opción inválida. Por favor, inténtalo de nuevo.")
+            pausar_pantalla()
+
+def pausar_pantalla():
+    input("Presiona Enter para continuar...")   
 
 if __name__ == "__main__":
     main()
